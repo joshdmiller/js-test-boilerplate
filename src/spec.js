2e9ed7329d8384edeba456c2283d92e4944020ec
kaxla <kaylas.email.address@gmail.com>
2016-12-09 14:41:25 -0800

    solution and ES6 solution

--- src/spec.js
+++ src/spec.js
@@ -1,30 +1,56 @@
 import test from 'tape';
-import { double, doubleXTimes, doubleEach } from './index';
+import { Animal, Reptile, Primate, Human } from './index';
 
-test( 'double fn', function ( test ) {
-  const actual = double( 5 );
-  const expected = 10;
-
-  test.equal( actual, expected, 'should double the value' );
-
+test('animal', function(test) {
+  const animal = new Animal();
+  test.ok(animal instanceof Animal, 'should create an instance of Animal');
   test.end();
 });
 
-test( 'doubleXTimes', function ( test ) {
-  const actual = doubleXTimes( 5, 3 );
-  const expected = 40;
+test('speak', function(test) {
+  const animal = new Animal();
+  const expected = 'generic sound';
+  const actual = animal.speak();
 
-  test.equal( actual, expected, 'should double 5 three times' );
-
+  test.equal(actual, expected, 'should make a generic sound when it speaks');
   test.end();
 });
 
-test( 'doubleEach', function ( test ) {
-  const actual = doubleEach([ 0, 1, 2 ]);
-  const expected = [ 0, 2, 4 ];
+test('inheritance', function(test) {
+  let actual, expected;
+  const reptile = new Reptile();
+  test.ok(reptile instanceof Reptile, 'should be an instance of Reptile');
+  test.ok(reptile instanceof Animal, 'should be an instance of Animal');
 
-  test.deepEqual( actual, expected, 'should double each in the array' );
+  expected = 'rawr';
+  actual = reptile.speak();
 
+  test.equal(actual, expected, 'should make a reptile sound when it speaks');
+
+  const primate = new Primate();
+
+  test.ok(primate instanceof Primate, 'should be an instance of primate');
+  test.ok(primate instanceof Animal, 'should be an instance of animal');
+
+  expected = "primate noise";
+  actual = primate.speak();
   test.end();
 });
+
+test('double inheritance', function(test) {
+  let actual, expected;
+
+  const human = new Human();
+
+  test.ok(human instanceof Human, 'should be an instance of human');
+  test.ok(human instanceof Primate, 'should be an instance of Primate');
+  test.ok(human instanceof Animal, 'should be an instance of animal');
+
+  const message = 'hello';
+  expected = message;
+  actual = human.speak(message);
+  test.equal(actual, expected, 'should speak what it is asked to speak');
+  test.end();
+})
+
 

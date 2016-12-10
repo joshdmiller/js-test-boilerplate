8de507a2d7ef7267b7febd5c7fa917171487aa06
Craig Rodrigues <PagingCraig@gmail.com>
2016-12-10 14:17:36 -0500

    save

--- src/index.js
+++ src/index.js
@@ -29,12 +29,10 @@ export function Animal() {
   this.speak = 'generic sound';
 }
 
-Reptile.prototype = Object.create(Animal.prototype);
-
 export function Reptile() {
   Animal.apply(this);
   this.speak = () => console.log('Snakes be on a plane!');
 }
 
-//Reptile.prototype = Object.create(Animal.prototype);
+Reptile.prototype = Object.create(Animal.prototype);
 

33ffcc61def59131e43e3910ef5f1615fdae7f3b
sheriffderek <admin@sheriffderek.consulting>
2016-12-11 19:15:10 -0800

    Trying version 2

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import stampit from 'stampit';
 //
 
 export const Animal = stampit();
+
+// I don't understand why there is an object being passed in here...
+const SelfAwareStamp = stampit.init(({ instance, stamp }) => {
+  instance.getStamp = () => stamp;
+});
+
+export const SelfAwareAnimal = Animal.compose(SelfAwareStamp);
--- src/spec.js
+++ src/spec.js
@@ -1,12 +1,36 @@
 import test from 'tape';
-import { Animal } from './index';
+import { Animal, SelfAwareAnimal } from './index';
 import stampit from 'stampit';
-import isStamp from 'stampit/isStamp';
+// import isStamp from 'stampit/isStamp';
 
-test('Animal exists', function(test) {
-  const animal = new Animal();
+test('Can create stamps from SelfAwareAnimal', function(test) {
+  const animal = SelfAwareAnimal();
 
-  test.ok( animal instanceof Animal, 'should create an instance of Animal' );
+  test.deepEqual( animal.getStamp(), SelfAwareAnimal, 'should create an "instance"/stamp from SelfAwareAnimal' );
+
+  test.end();
+});
+
+test('More "inheritance" stamp tests', function(test) {
+  let actual, expected;
+
+  const reptile = new Reptile();
+
+  test.ok( reptile instanceof Reptile, 'should be an instance of Reptile' );
+  test.ok( reptile instanceof Animal, 'should be an instance of Animal' );
+
+  expected = Reptile.SOUND;
+  actual = reptile.speak();
+  test.equal( actual, expected, 'should make a reptile sound when it speaks' );
+
+  const primate = new Primate();
+
+  test.ok( primate instanceof Primate, 'should be an instance of Primate' );
+  test.ok( primate instanceof Animal, 'should be an instance of Animal' );
+
+  expected = Primate.SOUND;
+  actual = primate.speak();
+  test.equal( actual, expected, 'should make a primate sound when it speaks' );
 
   test.end();
 });

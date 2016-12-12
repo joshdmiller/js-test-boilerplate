5cc3fbd23410fa48ac9c0a8996117bfb3d297b6d
sheriffderek <admin@sheriffderek.consulting>
2016-12-11 21:27:31 -0800

    save…

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import stampit from 'stampit';
 //
-
-export const Animal = stampit();
-
-// I don't understand why there is an object being passed in here...
-const SelfAwareStamp = stampit.init(({ instance, stamp }) => {
-  instance.getStamp = () => stamp;
+export const Animal = stampit({
+  props: {
+    SOUND: "Generic sound"
+  },
+  methods: {
+    speak(sound) {
+      return sound;
+    }
+  }
 });
 
-export const SelfAwareAnimal = Animal.compose(SelfAwareStamp);
+
+export const Reptile = stampit().compose(Animal);
+
+export const Primate = stampit().compose(Animal);
+
+export const Human = stampit().compose(Primate);
--- src/spec.js
+++ src/spec.js
@@ -1,29 +1,27 @@
 import test from 'tape';
-import { Animal, SelfAwareAnimal } from './index';
+import { Animal, Reptile } from './index';
 import stampit from 'stampit';
 // import isStamp from 'stampit/isStamp';
 
-test('Can create stamps from SelfAwareAnimal', function(test) {
-  const animal = SelfAwareAnimal();
-
-  test.deepEqual( animal.getStamp(), SelfAwareAnimal, 'should create an "instance"/stamp from SelfAwareAnimal' );
-
+test('Can create instances from Animal stamp', function(test) {
+  const animal = Animal();
+  test.ok( typeof animal.speak == 'function', 'animal sounds like an Animal' );
   test.end();
 });
 
 test('More "inheritance" stamp tests', function(test) {
   let actual, expected;
 
-  const reptile = new Reptile();
+  const reptile = Reptile();
 
-  test.ok( reptile instanceof Reptile, 'should be an instance of Reptile' );
-  test.ok( reptile instanceof Animal, 'should be an instance of Animal' );
+  test.ok( typeof reptile.speak("I'm a reptile") == "I'm a reptile", 'should be an instance of Reptile' );
+  test.ok( typeof reptile.speak == 'function', 'should be an instance of Animal' );
 
   expected = Reptile.SOUND;
-  actual = reptile.speak();
+  actual = reptile.speak(expected);
   test.equal( actual, expected, 'should make a reptile sound when it speaks' );
 
-  const primate = new Primate();
+  const primate = Primate();
 
   test.ok( primate instanceof Primate, 'should be an instance of Primate' );
   test.ok( primate instanceof Animal, 'should be an instance of Animal' );

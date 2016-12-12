9fc1cb7b9d5619e993c9267819e42a9a6ccb7472
oddswop <yvonne@geared.net.au>
2016-12-13 06:47:56 +1100

    Lesson 1 - constructors, prototypes and inheritance

--- src/index.js
+++ src/index.js
@@ -24,3 +24,71 @@ export function doubleEach ( arr ) {
   return result;
 }
 
+// start simple
+
+//export function triple (x) {
+//    return x * 3;
+//}
+//
+//export function tripleEach (arr) {
+//    let result = arr.map (triple);
+//    return result;
+//}
+
+// lesson 1 - activity make Animal work.  Constructors and Prototypes
+// what I did:
+//
+//export function Animal ( animal,speak ) {
+//    this.speak = function(){
+//        return "generic sound";
+//    }
+//    
+//}
+
+export function Animal (){
+}
+
+Animal.prototype.speak = function(){
+    return 'generic sound';
+}
+
+// create new constructors with prototypes that inherit from Animal Reptile and Primate.  Overwrite the speak method for each.
+
+export function Reptile(){
+//    let SOUND = 'hiss';
+}
+
+Reptile.prototype = new Animal(); // inherit Animal properties if you get created
+Reptile.prototype.constructor = Reptile; // Make a valid reptile which is a new animal
+Reptile.SOUND = 'hissss';
+// you can create a function same as parent 
+
+Reptile.prototype.speak = function(){
+    return Reptile.SOUND;
+}
+
+export function Primate(){
+    }
+
+Primate.prototype = new Animal();
+Primate.prototype.constructor = Primate;
+Primate.SOUND = "bongos";
+
+Primate.prototype.speak = function (){
+    return Primate.SOUND;
+}
+
+
+// Create a human
+
+export function Human(){
+    
+}
+
+Human.prototype = new Primate();
+Human.prototype.constructor = Human;
+Human.message = 'hello';
+
+Human.prototype.speak = function(){
+    return Human.message;
+}

e0da8a04f071e30f7d7c807a7904eb276b175688
sheriffderek <admin@sheriffderek.consulting>
2016-12-11 17:48:47 -0800

    Reset / cleanup

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
 import stampit from 'stampit';
+//
 
-export function double ( x ) {
-  return x * 2;
-}
-
-export function doubleXTimes ( x, num ) {
-  let result = x;
-  for ( let i = 1; i <= num; i++ ) {
-    result = double( result );
-  }
-  return result;
-}
-
-export function doubleEach ( arr ) {
-  return arr.map( double );
-}
-
-export const Animal = stampit({
-  methods: {
-    speak(sound) {
-      return sound;
-    }
-  }
-});
-
-export const Reptile = stampit.compose(Animal);
-export const Primate = stampit.compose(Animal);
-export const Human = stampit.compose(Primate);
+export const Animal = stampit();
--- src/spec.js
+++ src/spec.js
@@ -1,43 +1,12 @@
 import test from 'tape';
-import { double, doubleXTimes, doubleEach, Animal, Reptile, Primate, Human} from './index';
+import { Animal } from './index';
 import stampit from 'stampit';
 import isStamp from 'stampit/isStamp';
 
-test( 'double fn', function ( test ) {
-  const actual = double( 5 );
-  const expected = 10;
-
-  test.equal( actual, expected, 'should double the value' );
-
-  test.end();
-});
-
-test( 'doubleXTimes', function ( test ) {
-  const actual = doubleXTimes( 5, 3 );
-  const expected = 40;
-
-  test.equal( actual, expected, 'should double 5 three times' );
-
-  test.end();
-});
-
-test( 'doubleEach', function ( test ) {
-  const actual = doubleEach([ 0, 1, 2 ]);
-  const expected = [ 0, 2, 4 ];
-
-  test.deepEqual( actual, expected, 'should double each in the array' );
-
-  test.end();
-});
-
 test('Animal exists', function(test) {
-  const animal = stampit().compose(Animal);
-  console.log( animal().speak('bark bark bark') );
-  // console.log('4. Animalxxxx', Animal.hasCreated(animal));
-  console.log('1. animal', animal);
-  // console.log('2. isStamp(Animal)', isStamp(Animal) );
-  // console.log('3. isstamp(animal)', isStamp(animal) );
-  test.ok( isStamp(Animal), 'Animal is a stamp' );
-  test.ok( '?', 'animal is an instance of Animal' );
+  const animal = new Animal();
+
+  test.ok( animal instanceof Animal, 'should create an instance of Animal' );
+
   test.end();
 });

723898704316a2d84e26bcd8dcdac132c368d5c8
Urmila Pradeep <urmilapradeep@sparkcentral.com>
2016-12-14 15:45:54 -0800

    First attempt at prototypes assignment

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-export function double ( x ) {
-  return x * 2;
+export function Animal () {
+
 }
 
-export function doubleXTimes ( x, num ) {
-  let result = x;
-
-  for ( let i = 1; i <= num; i++ ) {
-    // result = result * 2;
-    result = double( result );
-  }
-
-  return result;
+Animal.prototype.speak = function() {
+    return 'generic sound';
 }
 
-export function doubleEach ( arr ) {
-  let result = arr.map( double );
 
-  // const result = arr.map( x => double( x ) );
-  // const result = arr.map( function ( x ) {
-  //   return double( x );
-  // });
-
-  return result;
+export function Reptile() {
+    this.SOUND = 'reptile sound';
 }
 
+export function Primate() {
+    this.SOUND = 'primate sound'
+}
+
+Reptile.prototype = Object.create(Animal.prototype)
+
+Reptile.prototype.speak = function () {
+    return this.SOUND;
+};
+
+
+Primate.prototype = Object.create(Animal.prototype)
+
+Primate.prototype.speak = function (sound) {
+    return this.SOUND;
+}
--- src/spec.js
+++ src/spec.js
@@ -1,30 +1,79 @@
+// import test from 'tape';
+// import { double, doubleXTimes, doubleEach } from './index';
+//
+// test( 'double fn', function ( test ) {
+//   const actual = double( 5 );
+//   const expected = 10;
+//
+//   test.equal( actual, expected, 'should double the value' );
+//
+//   test.end();
+// });
+//
+// test( 'doubleXTimes', function ( test ) {
+//   const actual = doubleXTimes( 5, 3 );
+//   const expected = 40;
+//
+//   test.equal( actual, expected, 'should double 5 three times' );
+//
+//   test.end();
+// });
+//
+// test( 'doubleEach', function ( test ) {
+//   const actual = doubleEach([ 0, 1, 2 ]);
+//   const expected = [ 0, 2, 4 ];
+//
+//   test.deepEqual( actual, expected, 'should double each in the array' );
+//
+//   test.end();
+// });
+
 import test from 'tape';
-import { double, doubleXTimes, doubleEach } from './index';
+import {Animal, Reptile, Primate} from './index';
 
-test( 'double fn', function ( test ) {
-  const actual = double( 5 );
-  const expected = 10;
+test('instance', function (test) {
+    const animal = new Animal();
+    test.ok( animal instanceof Animal, 'should create an instance of Animal' );
 
-  test.equal( actual, expected, 'should double the value' );
-
-  test.end();
+    test.end();
 });
 
-test( 'doubleXTimes', function ( test ) {
-  const actual = doubleXTimes( 5, 3 );
-  const expected = 40;
+test('sound', function (test) {
 
-  test.equal( actual, expected, 'should double 5 three times' );
+    const animal = new Animal();
+    const expected = 'generic sound';
+    const actual = animal.speak('generic sound');
 
-  test.end();
+    test.equal( actual, expected, 'should make a generic sound when it speaks' );
+    test.end();
 });
 
-test( 'doubleEach', function ( test ) {
-  const actual = doubleEach([ 0, 1, 2 ]);
-  const expected = [ 0, 2, 4 ];
+test('reptile', function (test) {
 
-  test.deepEqual( actual, expected, 'should double each in the array' );
+    let actual, expected;
 
-  test.end();
+    const reptile = new Reptile();
+
+    test.ok( reptile instanceof Reptile, 'should be an instance of Reptile' );
+    test.ok( reptile instanceof Animal, 'should be an instance of Animal' );
+
+    expected = Reptile.SOUND;
+    actual = reptile.speak();
+    test.equal( actual, expected, 'should make a reptile sound when it speaks' );
+    test.end();
+})
+
+test('primate', function (test) {
+
+
+    const primate = new Primate();
+
+    test.ok( primate instanceof Primate, 'should be an instance of Primate' );
+    test.ok( primate instanceof Animal, 'should be an instance of Animal' );
+
+    expected = Primate.SOUND;
+    actual = primate.speak();
+    test.equal( actual, expected, 'should make a primate sound when it speaks' );
+
+    test.end();
 });
-
